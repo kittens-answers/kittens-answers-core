e6b631d6fbe1310c1182f06ac264179647db825c
Rychanya <rychanya@gmail.com>
2023-05-16 20:29:39 +0300

    🔨 add nox

---
+++ noxfile.py
@@ -0,0 +1,13 @@
+import nox
+
+
+@nox.session(python=False)
+def style(session: nox.Session):
+    session.run("poetry", "run", "isort", ".", external=True)
+    session.run("poetry", "run", "black", ".", external=True)
+    session.run("poetry", "run", "ruff", ".", external=True)
+
+
+@nox.session(python=False)
+def test(session: nox.Session):
+    session.run("poetry", "run", "pytest", external=True)
